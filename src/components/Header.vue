c15e7262f6fa3547a3adfd9c353c44566ff5d075
tony.lee <tony.lee@deradius.com>
2021-09-12 23:01:18 -0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+    <Button />
+  </header>
+</template>
+
+<script>
+  import Button from './Button';
+
+  export default {
+    name: 'Header',
+    components: {
+      Button
+    },
+    props: {
+      title: {
+        type: String,
+        default: "Helllo",
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    margin-bottom: 20px;
+  }
+</style>
